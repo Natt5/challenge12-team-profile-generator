a17b9a842df0656c1789268e1c02c6aeb7f73a9c
Natalie Bo <nataliabo@hotmail.co.uk>
2024-02-27 11:22:07 +0000

    feat:created manager, engineer, intern questions/answers logic.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
+
 const path = require("path");
 const fs = require("fs");
 
@@ -13,3 +14,25 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+const teamMembers = [];
+
+const managerAnswers = await inquirer.prompt([
+    { type: 'input', name: 'name', message: "What is the team manager's name?" },
+    { type: 'input', name: 'id', message: "What is the team manager's ID?" },
+    { type: 'input', name: 'email', message: "What is the team manager's email?" },
+    { type: 'input', name: 'officeNumber', message: "What is the team manager's office number?" }
+]);
+
+const engineerAnswers = await inquirer.prompt([
+    { type: 'input', name: 'name', message: "What is the engineer's name?" },
+    { type: 'input', name: 'id', message: "What is the engineer's ID?" },
+    { type: 'input', name: 'email', message: "What is the engineer's email?" },
+    { type: 'input', name: 'github', message: "What is the engineer's GitHub username?" }
+]);
+
+const internAnswers = await inquirer.prompt([
+    { type: 'input', name: 'name', message: "What is the intern's name?" },
+    { type: 'input', name: 'id', message: "What is the intern's ID?" },
+    { type: 'input', name: 'email', message: "What is the intern's email?" },
+    { type: 'input', name: 'school', message: "What is the intern's school?" }
+]);
